a70612c4b88598d39a3bd9c71f4bd35506b93b7a
koki-ok <consideration.days.s@gmail.com>
2022-02-09 22:21:26 +0900

    rubocopによる自動修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,12 +5,9 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
-
   private
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
