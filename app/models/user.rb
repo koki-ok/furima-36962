6f0348b97aab6ace06b042e3edb0caffeb7e37a5
koki-ok <consideration.days.s@gmail.com>
2022-02-13 10:36:41 +0900

    モデルの単体テストコードの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,17 @@ class User < ApplicationRecord
   validates :last_name_kana,  presence: true
   validates :first_name_kana, presence: true
   validates :birth_day,       presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）での入力が必要です' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カタカナでの入力が必要です' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
