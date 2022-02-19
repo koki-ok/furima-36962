ecab6b9ed6f253cb07b0896972fcd4b2e0227990
koki-ok <consideration.days.s@gmail.com>
2022-02-19 18:23:38 +0900

    ActiveHash::Baseのデータ作成・アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_condition
+  belongs_to :burden_of_shipping_charge
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
   belongs_to :user
  # has_one :purchase
   has_one_attached :image
