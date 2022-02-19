d7d1b539f7459785c0cdcc9ce8de2015955c7462
koki-ok <consideration.days.s@gmail.com>
2022-02-19 18:59:23 +0900

    collection_selectに関する編集(itemsコントローラー、new.html.erb)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,20 @@ class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index, :show]
 
   def index
-    @items = Item.all
+    @items = Items.all
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   # ユーザー管理機能の時点では不要なためコメントアウト
@@ -16,6 +25,11 @@ class ItemsController < ApplicationController
   def message_params
     params.require(:item).permit(:image).merge(user_id: current_user.id)
   end
+
+  def item_params
+    params.require(:item).permit(:product_name, :product_description, :category_id, :product_condition_id, :burden_of_shipping_charge_id, :shipping_area_id, :days_to_ship_id, :price, :user_id)
+  end
+
   # def move_to_index
     # redirect_to action: :index unless user_signed_in?
   # end
