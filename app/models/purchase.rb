2c9f84f91cb6707775cd9fae2414970925bc0cd7
koki-ok <consideration.days.s@gmail.com>
2022-02-23 22:47:08 +0900

    ActiveHash（都道府県の部分）の導入

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,10 @@
 class Purchase < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_area
+
   belongs_to :user
   belongs_to :item
   has_one :address
+
+  validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" } 
 end
