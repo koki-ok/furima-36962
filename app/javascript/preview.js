2cc4041e47dd206ec28ebcaa28eb83c45ceb8340
koki-ok <consideration.days.s@gmail.com>
2022-05-23 19:22:02 +0900

    input要素で値の変化が起きたら呼び出される関数の定義

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページのフォームを取得
-  const postForm = document.getElementById('item-image');
+  const postForm = document.getElementById('new_post');
   // プレビューを表示するためのスペースを取得
-  const previewList = document.getElementById('previews')
+  const previewList = document.getElementById('previews');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
   console.log("preview.jsが読み込まれました");
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function(e){
+    console.log("input要素で値の変化が起きました");
+  });
 });
